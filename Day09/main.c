f802fa41b5f7c5acd8645d82d08cb622a054329c
vbn94 <vbn_94@abv.bg>
2022-12-21 14:10:57 +0200

    Day9

--- Day09/main.c
+++ Day09/main.c
@@ -11,13 +11,10 @@ int maxNum(int num1, int num2){
 }
 
 int minNum(int num1, int num2){
-    int min;
     if (num1 < num2){
-        min = num1;
-    } else {
-        min = num2;
+        return num1;
     }
-    return min;
+    return num2;
 }
 
 double parallelResistance(double R1, double R2){
@@ -25,8 +22,51 @@ double parallelResistance(double R1, double R2){
     return result;
 }
 
+double parallelResistance3(double R1, double R2, double R3){
+    double r12 = parallelResistance(R1, R2);
+    double r123 = parallelResistance(r12, R3);
+    return r123;
+}
 
-int main(){
+char oddeven(int num){
+    char ch;
+    if (num % 2 == 0){
+        ch = 'e';
+    } else {
+        ch = 'o';
+    }
+    return ch;
+}
+
+void printNumber(int num){
+    printf("Num = %d\n", num);
+}
+
+void driveHead(int dir){
+    if (0 == dir){
+        printf("Drive head DOWN!\n");
+    } else if (1 == dir){
+        printf("Drive head UP!\n");
+    }
+}
+
+int getNumber(){
+    return 567;
+    return 15;
+    return 56;
+}
+
+int main(void){
+    //driveHead(0);
+   // driveHead(1);
+    //driveHead(1);
+    //driveHead(0);
+    printf("%d\n", getNumber());
+    /*
+    printNumber(23);
+    printNumber(3456);
+    printf("%c\n", oddeven(10)); // e
+    printf("%c\n", oddeven(15)); // o
     double r1 = 4.5;
     double r2 = 5;
     double r12 = parallelResistance(r1, r2);
