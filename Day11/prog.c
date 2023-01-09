cc0d237ab2f8390bc189afc76daa16e05ccf5cf1
vbn94 <vbn_94@abv.bg>
2023-01-09 16:09:47 +0200

    Day11

--- Day11/prog.c
+++ Day11/prog.c
@@ -2,11 +2,24 @@
 
 #define FORI(start, end, step) for(int i = start; i <=end; i+=step)
 #define PRINTNUM(num) printf("%d\n", num)
+#define CHECKBIT(mask, bit) !!((mask) & (1 << (bit)))
+#define SETBIT(mask, bit) mask |= (1 << (bit))
+#define CLEARBIT(mask, bit) mask &= ~(1 << (bit))
+#define TOGGLEBIT(mask, bit) mask ^= (1 << (bit))
 
-int main(){
-    FORI(10, 100, 5){
-        PRINTNUM(i);
+void printBits(int mask){
+    for (int i = 31; i >= 0; i--){
+        printf("%d", CHECKBIT(mask, i));
     }
     printf("\n");
+}
+
+int main(){
+    int m = 0;
+    SETBIT(m, 3);
+    SETBIT(m, 0);
+    SETBIT(m, 10);
+    SETBIT(m, 21);
+    printBits(m);
     return 0;
 }
